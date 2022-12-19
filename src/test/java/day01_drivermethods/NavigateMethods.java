6f17c0e2582493f172932aeb318df04feef3ea75
Elif Calık <beyaztahta0@gmail.com>
2022-12-19 13:24:59 +0300

    a

--- src/test/java/day01_drivermethods/NavigateMethods.java
+++ src/test/java/day01_drivermethods/NavigateMethods.java
@@ -5,6 +5,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class NavigateMethods {
     public static void main(String[] args) {
+        /*
+        Sayfalar arası gecis varsa mutlaka kullanılmalı "navigate to".implicitlyWait() metodunu biz en basta kullaniriz.
+        Cunku herhangi bir internet yavasligi vs. durumunda bazen daha site tam acilmadan bizim asagida yazdigimiz diger testler FAIL olur.
+         Bunun onune gecmek icin (sitenin tum HTML elementleri tam yuklenmeden teste baslamasin diye) en basa bunu koyariz.
+        sleep() ise bizim yazdigimiz test kodlari arasinda bekleme yapsin diye yazdigimiz bir kod.
+         */
 
         System.setProperty("webdriver.chrome.driver","src/resources/chromedrivers/chromedriver.exe");
         WebDriver driver=new ChromeDriver();
@@ -35,31 +41,6 @@ public class NavigateMethods {
 
         //Ve sayfayı kapatalım.
         driver.close();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     }
